7fa6cb6e2619583ebb502eaea3149644f7f798d7
clarence <tegumclarence@gmail.com>
2023-05-23 14:47:39 +0100

    added js configuration

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const hamberger = document.getElementById('hamberger');
 const menu = document.getElementById('menu');
 
 const mobileMenu = () => {
-    menu.classList.toggle('active');
-    hamberger.classList.toggle('close');
-}
+  menu.classList.toggle('active');
+  hamberger.classList.toggle('close');
+};
 
-hamberger.addEventListener('click', mobileMenu)
+hamberger.addEventListener('click', mobileMenu);
 menu.addEventListener('click', mobileMenu);
